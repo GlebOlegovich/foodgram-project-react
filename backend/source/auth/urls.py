b39e02d272a7643dd4a4cceebab4ec9042a9af60
GlebOlegovich <godleib@yandex.ru>
2022-03-04 15:23:52 +0300

    Добавил смену пароля.

--- backend/source/auth/urls.py
+++ backend/source/auth/urls.py
@@ -1,8 +1,10 @@
 from django.urls import include, path
 
+# from .views import UpdatePassword
+
 app_name = 'auth'
 
-# НЕ СДЕЛАНО ИЗМЕНЕНИЕ ПАРОЛЯ!
 urlpatterns = [
     path('', include('djoser.urls.authtoken')),
+    # path('set_password/', UpdatePassword.as_view(), name=('set_password')),
 ]
--- backend/source/users/urls.py
+++ backend/source/users/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from .views import UserViewSet
 # from .views import registration_user
+from auth.views import UpdatePassword
 
 app_name = 'users'
 
@@ -12,6 +13,7 @@ router = DefaultRouter()
 router.register('', UserViewSet, basename='users')
 
 urlpatterns = [
+    path('set_password/', UpdatePassword.as_view(), name=('set_password')),
     path('', include(router.urls)),
 
 ]
